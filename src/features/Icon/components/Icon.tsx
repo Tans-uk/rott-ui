5a3f8aa6d35be37880e7e83e96825c9bf1c90620
Doğukan Tansuk <dogukantansuk@gmail.com>
2025-08-04 13:30:19 +0300

    refactor(icons): standardize SVG icon formatting and update fill mode in Icon component

--- src/features/Icon/components/Icon.tsx
+++ src/features/Icon/components/Icon.tsx
@@ -5,42 +5,42 @@ import {colorFromVariant, display} from '../../../utils'
 import {Item} from '../../Item'
 import type {IconProps} from '../models'
 
+import {SvgProps} from 'react-native-svg'
+
 export const Icon: FC<IconProps> = ({
   name,
   width = 16,
   height = 16,
   variant = 'white',
-  mode = 'stroke',
+  mode = 'fill',
   noStroke,
   strokeWidth = mode === 'fill' || noStroke ? 0 : 1,
   fill,
   stroke,
+  color,
   strokeLinecap,
   strokeLinejoin,
-  color,
   opacity,
   ...props
 }) => {
-  const IconComponent = themeConfig.icons[name]?.default
+  const IconComponent = themeConfig.icons[name]
+  if (mode === 'fill') fill = color ?? colorFromVariant(variant)
+  else if (mode === 'stroke') stroke = color ?? colorFromVariant(variant)
   if (!IconComponent) return null
 
   // Convert opacity to number if it's a string, ensuring type compatibility with ItemProps
   const itemOpacity = typeof opacity === 'string' ? parseFloat(opacity) : opacity
 
-  if (mode === 'fill') fill = color ?? colorFromVariant(variant)
-  else if (mode === 'stroke') stroke = color ?? colorFromVariant(variant)
-
   return (
     <Item {...props} opacity={itemOpacity}>
-      <IconComponent
+      <IconComponent.default
         fill={fill}
         stroke={stroke}
         strokeWidth={strokeWidth}
-        strokeLinecap={strokeLinecap}
-        strokeLinejoin={strokeLinejoin}
         width={display.px(width)}
         height={display.px(height)}
-        opacity={opacity}
+        strokeLinecap={strokeLinecap}
+        strokeLinejoin={strokeLinejoin}
       />
     </Item>
   )
