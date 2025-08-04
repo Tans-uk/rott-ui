957872d80daae1359ec23612804c2a25c1a94086
Doğukan Tansuk <dogukantansuk@gmail.com>
2025-08-04 14:26:00 +0300

    chore(release): bump version to 0.1.21 and add new Yarn version file

--- src/features/Icon/components/Icon.tsx
+++ src/features/Icon/components/Icon.tsx
@@ -5,8 +5,6 @@ import {colorFromVariant, display} from '../../../utils'
 import {Item} from '../../Item'
 import type {IconProps} from '../models'
 
-import {SvgProps} from 'react-native-svg'
-
 export const Icon: FC<IconProps> = ({
   name,
   width = 16,
@@ -24,9 +22,10 @@ export const Icon: FC<IconProps> = ({
   ...props
 }) => {
   const IconComponent = themeConfig.icons[name]
+  if (!IconComponent) return null
+
   if (mode === 'fill') fill = color ?? colorFromVariant(variant)
   else if (mode === 'stroke') stroke = color ?? colorFromVariant(variant)
-  if (!IconComponent) return null
 
   // Convert opacity to number if it's a string, ensuring type compatibility with ItemProps
   const itemOpacity = typeof opacity === 'string' ? parseFloat(opacity) : opacity
