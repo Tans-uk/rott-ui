47ed45862aa47c25f58ad320652ab8121e8902f6
Doğukan Tansuk <dogukantansuk@gmail.com>
2025-08-27 16:19:46 +0300

    feat(theme): update color definitions and improve component styling

--- jest.setup.ts
+++ jest.setup.ts
@@ -316,10 +316,56 @@ jest.mock('./src/theme', () => {
         }
       ),
       colors: {
-        primary: '#00A9CE',
-        white: '#FFFFFF',
-        black: '#111111',
-        warning: '#FF7518',
+        // PRIMARY
+        primary: 'rgba(0, 169, 206, 1)',
+        secondary: 'rgba(255, 199, 44, 1)',
+        'grey-900': 'rgba(34, 63, 70, 1)',
+        'grey-800': 'rgba(61, 88, 94, 1)',
+        'grey-200': 'rgba(161, 173, 175, 1)',
+        'grey-100': 'rgba(234, 239, 240, 1)',
+        white: 'rgba(255, 255, 255, 1)',
+        black: 'rgba(17, 17, 17, 1)',
+
+        // SEMANTIC
+        danger: 'rgba(246, 83, 83, 1)',
+        success: 'rgba(63, 182, 24, 1)',
+        info: 'rgba(63, 182, 210, 1)',
+        warning: 'rgba(255, 117, 24, 1)',
+        mint: 'rgba(32, 150, 110, 1)',
+
+        'success-notification': 'rgba(72, 193, 181, 1)',
+        'danger-notification': 'rgba(209, 94, 83, 1)',
+        'warning-notification': 'rgba(255, 173, 50, 1)',
+        'info-notification': 'rgba(77, 175, 234, 1)',
+
+        // NEUTRAL/ALPHA
+        'neutral-alpha-900': 'rgba(34, 63, 70, 0.9)',
+        'neutral-alpha-700': 'rgba(34, 63, 70, 0.75)',
+        'neutral-alpha-400': 'rgba(34, 63, 70, 0.40)',
+        'neutral-alpha-300': 'rgba(34, 63, 70, 0.30)',
+        'neutral-alpha-200': 'rgba(34, 63, 70, 0.15)',
+        'neutral-alpha-100': 'rgba(34, 63, 70, 0.10)',
+        'neutral-shadow-300': 'rgba(6, 14, 16, 0.30)',
+
+        'neutral-red-alpha-300': 'rgba(228, 65, 96, 0.30)',
+        'neutral-green-alpha-300': 'rgba(68, 238, 162, 0.30)',
+        'neutral-grey-alpha-200': 'rgba(234, 239, 240, 0.15)',
+        'neutral-blue-soft': 'rgba(130, 222, 243, 1)',
+        'neutral-blue-alpha': 'rgba(99, 115, 129, 1)',
+
+        // GRADIENT
+        'primary-outline': 'transparent',
+        'secondary-outline': 'transparent',
+        'success-outline': 'transparent',
+        'info-outline': 'transparent',
+        'warning-outline': 'transparent',
+        'danger-outline': 'transparent',
+        'white-outline': 'transparent',
+
+        // SEPERATOR COLOR
+        'seperator-grey-200': 'rgba(79, 100, 104, 1)',
+
+        transparent: 'rgba(0, 0, 0, 0)',
       },
     },
   }
