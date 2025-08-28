e2190c21a137fbb35cf7d6fadad246558cd53fd5
odling <o.uludag1@gmail.com>
2025-08-28 13:13:39 +0300

    refactor(i18n): remove Turkish language type from translationLanguageTypes export

--- src/libs/i18n/utils/models/translationLanguageTypes.ts
+++ src/libs/i18n/utils/models/translationLanguageTypes.ts
@@ -1,4 +1,3 @@
 import type en from '../../en-US.json'
-import type tr from '../../tr-TR.json'
 
-export type TranslationLanguageTypes = keyof typeof en | keyof typeof tr
+export type TranslationLanguageTypes = keyof typeof en
